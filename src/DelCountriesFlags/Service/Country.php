1166f288f24dcfc4786fd267b9aaa7a7b7f37ff5
delboy1978uk <delboy1978uk@gmail.com>
2012-11-04 20:19:09 +0000

    connect country service to country mapper

--- src/DelCountriesFlags/Service/Country.php
+++ src/DelCountriesFlags/Service/Country.php
@@ -29,7 +29,7 @@ class Country extends EventProvider implements ServiceManagerAwareInterface
      */
     public function getCountry($id)
     {
-    	return $this->getCountryMapper()->findByID($id);
+    	return $this->getCountryMapper()->getCountryByID($id);
     }
 
     /**
