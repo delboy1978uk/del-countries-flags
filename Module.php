3f6f05607380601df3297fa0c2fd358205db2166
delboy1978uk <delboy1978uk@gmail.com>
2012-11-04 18:25:36 +0000

    remove service config empty array

--- Module.php
+++ Module.php
@@ -23,12 +23,7 @@ class Module
         return include __DIR__ . '/config/module.config.php';
     }
     
-    public function getServiceConfig()
-    {
-    	return array(
-    	
-    	);
-    }
+    
 
    /* public function getViewHelperConfig()
     {
