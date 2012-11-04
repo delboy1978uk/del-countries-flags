7a4f518937a0389c73bf58701fbdc9f44846c392
delboy1978uk <delboy1978uk@gmail.com>
2012-11-04 18:24:31 +0000

    using db mapper in service

--- Module.php
+++ Module.php
@@ -22,6 +22,13 @@ class Module
     {
         return include __DIR__ . '/config/module.config.php';
     }
+    
+    public function getServiceConfig()
+    {
+    	return array(
+    	
+    	);
+    }
 
    /* public function getViewHelperConfig()
     {
