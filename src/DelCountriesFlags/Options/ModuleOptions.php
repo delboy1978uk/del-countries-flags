c2a33916f3223555b89238dcc90adf9b5fdefcfa
delboy1978uk <delboy1978uk@gmail.com>
2012-11-07 21:49:25 +0000

    module options

--- src/DelCountriesFlags/Options/ModuleOptions.php
+++ src/DelCountriesFlags/Options/ModuleOptions.php
@@ -11,7 +11,14 @@ class ModuleOptions extends AbstractOptions implements CountryServiceOptionsInte
      * @var string
      */
     protected $countryEntityClass = 'DelCountriesFlags\Entity\Country';
+    
+    /** @var string */
+    protected $countryNameViewTemplate = 'del-countries-flags/country.phtml';
 
+    /** @var string */
+    protected $countryFlagViewTemplate = 'del-countries-flags/flag.phtml';
+    
+    
     /**
      * set country entity class name
      *
@@ -32,6 +39,50 @@ class ModuleOptions extends AbstractOptions implements CountryServiceOptionsInte
     public function getCountryEntityClass()
     {
         return $this->countryEntityClass;
+    }
+    
+	/**
+     * set the view template for the country
+     *
+     * @param string $countryNameViewTemplate
+     * @return ModuleOptions
+     */
+    public function setCountryNameViewTemplate($countrynameViewTemplate)
+    {
+        $this->countryNameViewTemplate = $countrynameViewTemplate;
+        return $this;
+    }
+
+    /**
+     * get the view template for the image
+     *
+     * @return string
+     */
+    public function getCountryNameViewTemplate()
+    {
+        return $this->countryNameViewTemplate;
+    }
+    
+	/**
+     * set the view template for the country
+     *
+     * @param string $countryFlagViewTemplate
+     * @return ModuleOptions
+     */
+    public function setCountryFlagViewTemplate($countryFlagViewTemplate)
+    {
+        $this->countryFlagViewTemplate = $countryFlagViewTemplate;
+        return $this;
+    }
+
+    /**
+     * get the view template for the image
+     *
+     * @return string
+     */
+    public function getCountryFlagViewTemplate()
+    {
+        return $this->countryFlagViewTemplate;
     }
 
     
